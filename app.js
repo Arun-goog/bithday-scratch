369f73c76904cc8010153518ba9b4ec4dbff1692
Arun-goog <63851187+Arun-goog@users.noreply.github.com>
2023-12-27 13:46:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,40 +15,40 @@ $("#card2").wScratchPad({
 });
 $("#card3").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/3.jpeg`, // Background (image path or hex color).
+  bg: `Images/4.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
 
 $("#card4").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/Gpay_Card 4.png`, // Background (image path or hex color).
+  bg: `Images/5.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
 $("#card5").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/Gpay_Card 5.png`, // Background (image path or hex color).
+  bg: `Images/6.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
 
 $("#card6").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/Gpay_Card 6.png`, // Background (image path or hex color).
+  bg: `Images/7.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
 $("#card7").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/Gpay_Card 7.png`, // Background (image path or hex color).
+  bg: `Images/8.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
 
 $("#card8").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/Gpay_Card 8.png`, // Background (image path or hex color).
+  bg: `Images/9.jpeg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
